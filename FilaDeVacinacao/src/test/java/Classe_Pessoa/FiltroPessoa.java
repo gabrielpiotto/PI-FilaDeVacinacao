083f5a19173d8920df72b567f35b3c05b451cb31
User <User@192.168.0.17>
2021-06-06 19:31:11 -0300

    Adicionado relatório.

---
+++ FilaDeVacinacao/src/test/java/Classe_Pessoa/FiltroPessoa.java
@@ -0,0 +1,44 @@
+
+package Classe_Pessoa;
+
+
+
+
+public class FiltroPessoa {
+    
+    private String dataInicio;
+    private String dataFinal;
+    private int[] idade;
+
+    public FiltroPessoa(String dataInicio, String dataFinal, int[] idade) {
+        this.dataInicio = dataInicio;
+        this.dataFinal = dataFinal;
+        this.idade = idade;
+    }
+
+    public String getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(String dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public String getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(String dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    public int[] getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int[] idade) {
+        this.idade = idade;
+    }
+ 
+    
+}
